b4372fe3999813c38c36711f416c9a6a26c8954c
jevgeni86 <jevgeni.erehman@hotmail.com>
2017-04-23 20:38:35 +0200

    Update insert_number.php

--- insert_number.php
+++ insert_number.php
@@ -11,23 +11,18 @@ if (!$res){
 	return;//is already in db
 }
 
-$digifriends = array();
-
 #compute digifriend
 for ($i = 1; $i <= $X; $i++) {
     if (($i % 3) == 0){
 		$digifriend = $i+$i*$X;
-		array_push($digifriends, $digifriend);
 		mysql_query("insert into digifriend values (".$X.", $digifriend)");
 	}
     if (($i % 5) == 0){
 		$digifriend = $i+$i*round(sqrt($X))+1;
-		array_push($digifriends, $digifriend);
 		mysql_query("insert into digifriend values (".$X.", $digifriend)");
 	}
     if ((($i % 3) == 0) and (($i % 5) == 0)){
 		$digifriend = $i+$i*3*5*round(sqrt($X))+2;
-		array_push($digifriends, $digifriend);
 		mysql_query("insert into digifriend values (".$X.", $digifriend)");
 	}
 } 
